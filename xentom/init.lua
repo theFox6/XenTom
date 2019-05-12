8f4365ef095725b38168c9ec91e68f5f017bd356
Tomracer (orginal) <46093421+Tomracer@users.noreply.github.com>
2019-05-12 12:25:07 +0200

    Add files via upload

--- xentom/init.lua
+++ xentom/init.lua
@@ -6,8 +6,101 @@ minetest.register_craftitem("xentom:xentom_lump", {
 minetest.override_item("default:stone", {drop = {
     max_items = 1,
     items = {
-    {items = {'xentom:xentom_lump'}, rarity = 15},
+    {items = {'xentom:xentom_lump'}, rarity = 50},
     {items = {'default:cobble'}},
     }
 }})
+
+minetest.register_node( "xentom:block", {
+	description = "XenTom Block",
+	tile_images = { "xentom_block.png" },
+	is_ground_content = true,
+	groups = {cracky=3},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_tool("xentom:xentom_sword", {
+	description = "XenTom Sword",
+	inventory_image = "xentom_sword.png",
+	groups = {not_in_creative_inventory=1},
+	range = 7,
+	tool_capabilities = {
+		full_punch_interval = 01.0,
+		max_drop_level=1,
+		groupcaps={
+			snappy={times={[1]=0.90, [2]=0.30, [3]=0.50}, uses=123, maxlevel=5},
+		},
+		damage_groups = {fleshy=10},
+	},
+	sound = {breaks = "default_tool_breaks"},
+})
+
     
+minetest.register_craft({
+	output = "xentom:xentom_sword 1",
+	recipe = {
+		{"xentom:xentom_lump"},
+		{"xentom:xentom_lump"},
+		{"default:stick"}
+	}
+})
+
+minetest.register_tool("xentom:xentom_pickaxe", {
+    description = "XenTom Pickaxe",
+	inventory_image = "xentom_pickaxe.png",
+	groups = {not_in_creative_inventory=1},
+	range = 3.4,
+    tool_capabilities = {
+        full_punch_interval = 1.1,
+		max_drop_level=2,
+        groupcaps= {
+            cracky={times={[1]=2.00, [2]=1.50, [3]=0.76}, uses=400, maxlevel=5}
+        },
+        damage_groups = {fleshy=0.5},
+       },
+    sound = {breaks = "default_tool_breaks"},
+})
+
+minetest.register_craft({
+	output = "xentom:xentom_pickaxe 1",
+	recipe = {
+		{"xentom:xentom_lump","xentom:xentom_lump","xentom:xentom_lump"},
+		{"","default:stick",""},
+		{"","default:stick",""}
+	}
+})
+
+minetest.register_tool("xentom:xentom_shovel", {
+	description = "XenTom Shovel",
+	inventory_image = "xentom_shovel.png",
+	wield_image = "xentom_shovel.png^[transformR90",
+	groups = {not_in_creative_inventory=1},
+	range = 5,
+	tool_capabilities = {
+		full_punch_interval = 0.9,
+		max_drop_level=1,
+		groupcaps={
+			crumbly = {times={[1]=1.00, [2]=0.40, [3]=0.50}, uses=386, maxlevel=5},
+		},
+		damage_groups = {fleshy=7},
+	},
+	sound = {breaks = "default_tool_breaks"},
+})
+
+minetest.register_craft({
+	output = "xentom:xentom_shovel 1",
+	recipe = {
+		{"", "xentom:xentom_lump", ""},
+		{"", "default:stick", ""},
+		{"", "default:stick", ""}
+	}
+})
+
+minetest.register_craft({
+	output = 'xentom:block',
+	recipe = {
+		{'xentom:xentom_lump', 'xentom:xentom_lump', 'xentom:xentom_lump'},
+		{'xentom:xentom_lump', 'default:stone', 'xentom:xentom_lump'},
+		{'xentom:xentom_lump', 'xentom:xentom_lump', 'xentom:xentom_lump'},
+	}
+})
